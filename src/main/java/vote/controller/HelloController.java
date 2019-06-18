dd1f95d3bc27dd5b9a3e90943a101cb1e1553dd0
yanglin <1363909141@qq.com>
2019-06-18 11:43:59 +0800

    vote周实训模板

---
+++ src/main/java/vote/controller/HelloController.java
@@ -0,0 +1,15 @@
+package vote.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public ModelAndView hello() {
+		ModelAndView mv=new ModelAndView("hello");
+		return mv;
+	}
+}
