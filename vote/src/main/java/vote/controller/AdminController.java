315956b75a240449ead0c56c9d41029a0288e49b
gaopeigen <51895514+gaopeigen@users.noreply.github.com>
2019-06-19 10:22:38 +0800

    Add files via upload

---
+++ vote/src/main/java/vote/controller/AdminController.java
@@ -0,0 +1,15 @@
+package vote.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+	
+	@RequestMapping    //添加比赛投票
+	public void Addgame() {
+		
+	}
+}
---
+++ vote/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+世界如此美好，你怎么可以这样傻逼
+
+</body>
+</html>
